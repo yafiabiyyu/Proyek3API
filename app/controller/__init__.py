ff1c7dcc1ce21e9f9f8ded30442fda22babb7a8c
Abiyyu Yafi <luckyveri3@gmail.com>
2020-09-30 12:37:15 +0700

    menambahkan namespace alternatif

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -4,9 +4,11 @@ from flask_restplus import Api
 
 #import namespace
 from .controller_auth import api as auth_ns
+from .controller_alternatif import api as alternatif_ns
 
 
 controller = Blueprint('api', __name__)
 api = Api(controller, version='1.0', title="RESTAPI untuk proyek 3")
 
 api.add_namespace(auth_ns, path='/proyek/user')
+api.add_namespace(alternatif_ns,path="/proyek/data")
